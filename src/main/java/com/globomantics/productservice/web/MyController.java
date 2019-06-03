ae775d64ed8e5a2e09f254689ed82a497a510a7e
Satish.Marathe <marathesinpune@gmail.com>
2019-06-04 07:50:03 +1000

    added first working example of simple Junit

--- src/main/java/com/globomantics/productservice/web/MyController.java
+++ src/main/java/com/globomantics/productservice/web/MyController.java
@@ -1,8 +1,35 @@
 package com.globomantics.productservice.web;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.globomantics.productservice.model.MyOrder;
+import com.globomantics.productservice.model.MyOrderItem;
+import com.globomantics.productservice.model.MyOrderSummary;
+
 public class MyController {
 	
-	public void transform() {
+	public MyOrderSummary transform( MyOrder myOrder) {
+		MyOrderSummary myOrderSummaryResult = new MyOrderSummary();
 		
+		myOrderSummaryResult.setOrderNumber(myOrder.getOrderNumber());
+		
+		/** get the individual items **/
+		List <MyOrderItem> orderItemList = myOrder.getMyOrderItemList();
+		
+		int totalItemCount = 0;
+		BigDecimal totalItemPrice = new BigDecimal(0);
+		if(null != orderItemList && orderItemList.size() > 0) {
+			for(int z=0;z<orderItemList.size();z++) {
+				MyOrderItem myOrderItem = orderItemList.get(z);
+				totalItemCount = totalItemCount + myOrderItem.getQuantity();
+				System.out.println("myOrderItem.getSellingPrice() = " + myOrderItem.getSellingPrice());
+				System.out.println("myOrderItem.getQuantity() = " + myOrderItem.getQuantity());
+				totalItemPrice = totalItemPrice.add(myOrderItem.getSellingPrice().multiply(new BigDecimal(myOrderItem.getQuantity() )));
+				
+			}//for
+		}
+		myOrderSummaryResult.setItemCount(totalItemCount);
+		return myOrderSummaryResult;
 	}
 }
--- src/test/java/com/globomantics/productservice/web/MyControllerTest.java
+++ src/test/java/com/globomantics/productservice/web/MyControllerTest.java
@@ -2,8 +2,19 @@ package com.globomantics.productservice.web;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.UUID;
+
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.globomantics.productservice.model.MyOrder;
+import com.globomantics.productservice.model.MyOrderItem;
+import com.globomantics.productservice.model.MyOrderSummary;
+
+import junit.framework.Assert;
 
 public class MyControllerTest {
 	
@@ -21,8 +32,63 @@ public class MyControllerTest {
 	@Test
 	public void test_transform_success() {
 		/** note the method naming convention used which makes it easier to understand what we are testing **/
-		target.transform();
+		
+		String orderNumberFixture = UUID.randomUUID().toString();
+		
+		MyOrder myOrderFixture = new MyOrder();
+		myOrderFixture.setOrderNumber(orderNumberFixture);
+		
+		myOrderFixture.setMyOrderItemList(new LinkedList<MyOrderItem>());
+		
+		MyOrderItem myOrderItemFixture1 = new MyOrderItem();
+		MyOrderItem myOrderItemFixture2 = new MyOrderItem();
+		
+		myOrderItemFixture1.setQuantity(1);
+		myOrderItemFixture2.setQuantity(2);
+		
+		myOrderItemFixture1.setSellingPrice(new BigDecimal("10.00"));
+		myOrderItemFixture2.setSellingPrice(new BigDecimal("1.50"));
+		
+		myOrderFixture.getMyOrderItemList().add(myOrderItemFixture1);
+		myOrderFixture.getMyOrderItemList().add(myOrderItemFixture2);
+		
+		
+		/** variable is named as result **/
+		MyOrderSummary result = target.transform(myOrderFixture);
+		
+		/** note the deprecated way of asserting which is commented **/
+		assertNotNull(result);
+		//Assert.assertNotNull(result);
+		
+		assertEquals(orderNumberFixture, result.getOrderNumber());
+		
+		assertEquals(3,result.getItemCount());
+		
+		assertEquals(13,result.getTotalAmount());
 		
 	}
-
+	
+	/**
+	@Test(expected=IllegalArgumentException)
+	public void test_input_null() {
+		target.transform(null);
+	}
+	**/
+	
+	@Test
+	public void test_input_null() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			target.transform(null);
+		});
+		
+	}
+	
+	/**
+	@Test
+	@Ignore
+	public void test_something_future() {
+		// note the usage of @Ignore annotation to avoid running a test 
+		target.transform(null);
+	}
+	**/
 }
