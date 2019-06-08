6a4d34407e47153dd1865fa42a55c0bc707c06f1
Satish.Marathe <marathesinpune@gmail.com>
2019-06-08 10:52:49 +1000

    added some more negative test scenarios when we have a database access exception

--- src/test/java/com/globomantics/productservice/service/MyOrderServiceImplTest.java
+++ src/test/java/com/globomantics/productservice/service/MyOrderServiceImplTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.dao.DataAccessException;
 
+import com.globomantics.productservice.common.MyDataAccessException;
 import com.globomantics.productservice.common.MyServiceException;
 import com.globomantics.productservice.model.MyOrder;
 import com.globomantics.productservice.model.MyOrderSummary;
@@ -43,6 +44,39 @@ public class MyOrderServiceImplTest {
 		target.setTransformer(myTransformer);		
 	}
 	
+	/**
+	 * This test method is simulating a MyServiceException in the service class by ensuring the mocked DAO throws a DataAccessException.
+	 * This is an abnormal condition that we dont expect to occur but lets say the Database is down  
+	 * We want to ensure that code gracefully handles this scenario
+	 * In this case the Service class wraps the DataAccessException to a MyServiceException and throws it
+	 * So we test the service class against this exception 
+	 * @throws Exception
+	 */
+	@Test
+	public void test_getOrderSummary_throws_MyServiceException_when_Dao_throws_DataAccessException() throws  Exception{
+		/** specify what needs to be mocked **/
+		Mockito.mock(MyOrderDao.class);
+		
+		/** specify what should the DAO do and what is the condition **/
+		Mockito.when(mockOrderDao.findOrdersByCustomer(CUSTOMER_ID))
+		.thenThrow(MyDataAccessException.class);
+		
+		/** execute **/
+		/** here ordinarily the service method will return a List but in this scenario it is going to throw an exception 
+		 *  which is why we will not check for the return value at all
+		 **/
+		assertThrows(MyServiceException.class, () -> {
+			/** we will be making the actual execution call here **/
+			target.getOrderSummary(CUSTOMER_ID);
+		});
+	}
+	
+	/**
+	 * This test method is simulating a NPE in the service class by ensuring the mocked DAO returns a Null object.
+	 * This is an abnormal condition that we dont expect to occur so this is an edge condition but we do want to test it 
+	 * We want to ensure that code gracefully handles this scenario
+	 * @throws Exception
+	 */
 	@Test
 	public void test_getOrderSummary_throws_MyServiceException() throws Exception {			
 		/** we expect an ex ception to be thrown - an abnormal condition  **/
